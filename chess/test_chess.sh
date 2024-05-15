4fb3c14aa02c950e9b186917035ba7a75f0447e2
Yeongbin Kang <yeongbin.kang@kaist.ac.kr>
2024-05-15 20:50:05 +0900

    Add test script for module chess

---
+++ chess/test_chess.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3.10 ./wally.py --target chess.knight chess.rook chess.pawn --runner pytest --unit-test chess.test.pytest_chess -m
+python3.10 ./wally.py --target chess.knight chess.rook chess.pawn --runner unittest --unit-test chess.test.test_chess chess.test.test_chess2 -m
+python3.10 ./wally.py --target chess --runner unittest --unit-test chess.test  -m
