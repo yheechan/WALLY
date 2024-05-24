afcbc7453022e25854d83a5190a8ac02b4ea9eab
muyaho-universe <kimdaesk@gmail.com>
2024-05-24 20:45:18 +0900

    Update from unittest to pytest

--- examples/factorial/tests/test_factorial.py
+++ examples/factorial/tests/test_factorial.py
@@ -1,10 +1,11 @@
-import unittest
+import pytest
 from factorial import factorial
 
-class TestFactorial(unittest.TestCase):
-    def test_factorial_zero(self):
-        self.assertEqual(factorial(0), 1)
-    def test_factorial_one(self):
-        self.assertEqual(factorial(1), 1)  # error
-    def test_factorial_five(self):
-        self.assertEqual(factorial(5), 120) # error
+def test_factorial_zero():
+    assert factorial(0) == 1
+
+def test_factorial_one():
+    assert factorial(1) == 1
+
+def test_factorial_five():
+    assert factorial(5) == 120
